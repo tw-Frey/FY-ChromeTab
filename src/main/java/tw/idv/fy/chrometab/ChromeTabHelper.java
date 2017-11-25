9b0f04f5e27054b04a43371e0352fda911eb6156
Frey <tw-Frey@users.noreply.github.com>
2017-11-25 09:49:57 +0800

    新增導引安裝最新 Chrome; 檢查是否支援 Chrome Tab
    Signed-off-by: Frey <tw-Frey@users.noreply.github.com>

--- src/main/java/tw/idv/fy/chrometab/ChromeTabHelper.java
+++ src/main/java/tw/idv/fy/chrometab/ChromeTabHelper.java
@@ -1,17 +1,79 @@
 package tw.idv.fy.chrometab;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class ChromeTabHelper {
-    public static boolean LoadUri(Context context, Uri uri) {
-        try {
-            new CustomTabsIntent.Builder().build().launchUrl(context, uri);
-        } catch (Throwable e) {
-            e.printStackTrace();
-            return false;
+
+    private static final List<String> All_Possible_Chrome_Package;
+    private static final String STABLE_PACKAGE;
+    private static final String BETA_PACKAGE;
+    private static final String DEV_PACKAGE;
+    private static final String LOCAL_PACKAGE;
+    private static final Uri Chrome_MarketUri;
+    static {
+        All_Possible_Chrome_Package = Arrays.asList(
+                STABLE_PACKAGE = "com.android.chrome",
+                BETA_PACKAGE   = "com.chrome.beta",
+                DEV_PACKAGE    = "com.chrome.dev",
+                LOCAL_PACKAGE  = "com.google.android.apps.chrome"
+        );
+        Chrome_MarketUri = Uri.parse("market://details?id=" + STABLE_PACKAGE);
+    }
+    private static String sPackageNameToUse;
+
+    private static boolean CheckSupport(Context ctx) {
+        if (sPackageNameToUse == null) {
+            sPackageNameToUse = CustomTabsClient.getPackageName(ctx, All_Possible_Chrome_Package);
         }
-        return true;
+        return sPackageNameToUse != null;
+    }
+
+    private static void LoadUri(Context ctx, Uri uri) {
+        try {
+            CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+            customTabsIntent.intent.setPackage(sPackageNameToUse);
+            customTabsIntent.launchUrl(ctx, uri);
+        } catch (Throwable e) {
+            Toast.makeText(ctx, "Url or Browser has error.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        if (ctx instanceof Activity) ((Activity) ctx).finish();
+    }
+
+    public static void Open(Context context, Uri uri) {
+        context.startActivity(new Intent(context, Activity.class).setData(uri));
+    }
+
+    public static class Activity extends android.app.Activity {
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            final Uri uri = getIntent().getData();
+            if (CheckSupport(this)) {
+                LoadUri(this, uri);
+                return;
+            }
+            new AlertDialog.Builder(this, R.style.Theme_AppCompat_Dialog_Alert)
+                    .setPositiveButton(android.R.string.ok, (d, w) ->
+                            startActivity(new Intent(Intent.ACTION_VIEW, Chrome_MarketUri))
+                    )
+                    .setNegativeButton(android.R.string.no, (d, w) ->
+                            LoadUri(Activity.this, uri)
+                    )
+                    .setOnCancelListener(d -> finish())
+                    .setTitle("需要最新 Chrome 嗎？")
+                    .show();
+        }
     }
 }
