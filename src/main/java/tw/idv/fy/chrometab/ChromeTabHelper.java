b281582d39ce6a69cff940440724c943c0b9a0c1
Frey <tw-Frey@users.noreply.github.com>
2017-11-25 17:41:26 +0800

    提供客製部分 ChromeTab UI
    Signed-off-by: Frey <tw-Frey@users.noreply.github.com>

--- src/main/java/tw/idv/fy/chrometab/ChromeTabHelper.java
+++ src/main/java/tw/idv/fy/chrometab/ChromeTabHelper.java
@@ -3,16 +3,33 @@ package tw.idv.fy.chrometab;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.AnimRes;
+import android.support.annotation.NonNull;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
-@SuppressWarnings("unused")
+import static android.support.customtabs.CustomTabsIntent.EXTRA_EXIT_ANIMATION_BUNDLE;
+
+@SuppressWarnings({"unused", "WeakerAccess", "SpellCheckingInspection"})
 public class ChromeTabHelper {
+
+    public static final int NO_TITLE = CustomTabsIntent.NO_TITLE;
+    public static final int SHOW_PAGE_TITLE = CustomTabsIntent.SHOW_PAGE_TITLE;
+    public static final String EXTRA_TITLE_VISIBILITY_STATE = CustomTabsIntent.EXTRA_TITLE_VISIBILITY_STATE;
+    public static final String EXTRA_ENABLE_URLBAR_HIDING   = CustomTabsIntent.EXTRA_ENABLE_URLBAR_HIDING; // 預設: true(隱藏)
+    public static final String EXTRA_TOOLBAR_COLOR          = CustomTabsIntent.EXTRA_TOOLBAR_COLOR;
+
+    private static final String Extra_Key_OptionIntent  = "mOptionIntent";
+    private static final String Extra_Key_InAnimBundle  = "mStartAnimationBundle";
+    private static final String Extra_Key_OutAnimBundle = "mExitAnimationBundle";
+    private static final @AnimRes int Dummy_AnimRes = 0;
 
     private static final List<String> All_Possible_Chrome_Package;
     private static final String STABLE_PACKAGE;
@@ -30,6 +47,8 @@ public class ChromeTabHelper {
         Chrome_MarketUri = Uri.parse("market://details?id=" + STABLE_PACKAGE);
     }
     private static String sPackageNameToUse;
+    private static Bundle mStartAnimationBundle;
+    private static Bundle mExitAnimationBundle;
 
     private static boolean CheckSupport(Context ctx) {
         if (sPackageNameToUse == null) {
@@ -38,20 +57,64 @@ public class ChromeTabHelper {
         return sPackageNameToUse != null;
     }
 
-    private static void LoadUri(Context ctx, Uri uri) {
+    private static void LoadUri(Context ctx, Intent mIntent) {
         try {
-            CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+            CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
+                    .setStartAnimations(ctx, Dummy_AnimRes, Dummy_AnimRes) // must be, 否則無法覆蓋
+                    .setShowTitle(true)
+                    .build();
+            assert customTabsIntent.startAnimationBundle != null;
+            customTabsIntent.startAnimationBundle.putAll(mIntent.getBundleExtra(Extra_Key_InAnimBundle));
+            customTabsIntent.intent.putExtra(EXTRA_EXIT_ANIMATION_BUNDLE, mIntent.getBundleExtra(Extra_Key_OutAnimBundle));
+            customTabsIntent.intent.putExtras((Intent) mIntent.getParcelableExtra(Extra_Key_OptionIntent));
             customTabsIntent.intent.setPackage(sPackageNameToUse);
-            customTabsIntent.launchUrl(ctx, uri);
+            customTabsIntent.launchUrl(ctx, mIntent.getData());
         } catch (Throwable e) {
-            Toast.makeText(ctx, "Url or Browser has error.", Toast.LENGTH_LONG).show();
+            Toast.makeText(ctx, R.string.something_wrong, Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
         if (ctx instanceof Activity) ((Activity) ctx).finish();
     }
 
-    public static void Open(Context context, Uri uri) {
-        context.startActivity(new Intent(context, Activity.class).setData(uri));
+    public static Bundle DefaultEnterAnimationBundle(@NonNull Context context) {
+        if (mStartAnimationBundle == null) {
+            mStartAnimationBundle = ActivityOptionsCompat
+                    .makeCustomAnimation(context, android.R.anim.fade_in, android.R.anim.fade_out)
+                    .toBundle();
+        }
+        return mStartAnimationBundle;
+    }
+
+    public static Bundle DefaultExitAnimationBundle(@NonNull Context context) {
+        if (mExitAnimationBundle == null) {
+            mExitAnimationBundle = ActivityOptionsCompat
+                    .makeCustomAnimation(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                    .toBundle();
+        }
+        return mExitAnimationBundle;
+    }
+
+    public static void Open(@NonNull Context context, @NonNull Uri uri) {
+        Open(context, uri, DefaultEnterAnimationBundle(context), DefaultExitAnimationBundle(context));
+    }
+
+    public static void Open(@NonNull Context context, @NonNull Uri uri,
+                            @NonNull Bundle mStartAnimationBundle,
+                            @NonNull Bundle mExitAnimationBundle) {
+        Open(context, uri, mStartAnimationBundle, mExitAnimationBundle, new Intent());
+    }
+
+    public static void Open(@NonNull Context context, @NonNull Uri uri,
+                            @NonNull Bundle mStartAnimationBundle,
+                            @NonNull Bundle mExitAnimationBundle,
+                            @NonNull Intent mOptionIntent) {
+        context.startActivity(
+                new Intent(context, Activity.class)
+                        .setData(uri)
+                        .putExtra(Extra_Key_InAnimBundle , mStartAnimationBundle)
+                        .putExtra(Extra_Key_OutAnimBundle, mExitAnimationBundle)
+                        .putExtra(Extra_Key_OptionIntent , mOptionIntent)
+        );
     }
 
     public static class Activity extends android.app.Activity {
@@ -59,20 +122,20 @@ public class ChromeTabHelper {
         @Override
         protected void onResume() {
             super.onResume();
-            final Uri uri = getIntent().getData();
-            if (CheckSupport(this)) {
-                LoadUri(this, uri);
+            final Context ctx = this;
+            if (CheckSupport(ctx)) {
+                LoadUri(ctx, getIntent());
                 return;
             }
-            new AlertDialog.Builder(this, R.style.Theme_AppCompat_Dialog_Alert)
+            new AlertDialog.Builder(ctx, R.style.Theme_AppCompat_Dialog_Alert)
                     .setPositiveButton(android.R.string.ok, (d, w) ->
                             startActivity(new Intent(Intent.ACTION_VIEW, Chrome_MarketUri))
                     )
                     .setNegativeButton(android.R.string.no, (d, w) ->
-                            LoadUri(Activity.this, uri)
+                            LoadUri(ctx, getIntent())
                     )
                     .setOnCancelListener(d -> finish())
-                    .setTitle("需要最新 Chrome 嗎？")
+                    .setTitle(R.string.update_title)
                     .show();
         }
     }
